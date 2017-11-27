9938a39aa05d975af0e52b84a586bf09fde3ef69
Invixic <34034751+Invixic@users.noreply.github.com>
2017-11-27 20:45:47 +0100

    Add files via upload

--- BookTree.java
+++ BookTree.java
@@ -1,6 +1,15 @@
 
 public class BookTree {
 
-	//TODO: create a tree to organize books in RAM
+	//TODO: create a tree to organise books in RAM
+	Book root;
+	
+	public BookTree(Book futureRoot) {
+		
+		this.root = futureRoot;
+		
+	}
+	
+	
 	
 }
--- User.java
+++ User.java
@@ -4,4 +4,15 @@ public class User {
 	String[] lentBooks;
 	String[] orderedBooks;
 	
+	public User(String name) {
+		
+		this.userName = name;
+		
+	}
+	
+	/* public boolean searchOrderedBooks(String bookTitle) {
+	 * 
+	 *
+	 * 
+	 */
 }

cb1fea46809d2ab61c2bf96ed80dc9230d1f67eb
Relux-the-Relux <33958768+Relux-the-Relux@users.noreply.github.com>
2017-11-28 21:14:57 +0100

    k

--- BookTree.java
+++ BookTree.java
@@ -1,15 +1,88 @@
 
 public class BookTree {
 
-	//TODO: create a tree to organise books in RAM
 	Book root;
 	
-	public BookTree(Book futureRoot) {
-		
-		this.root = futureRoot;
-		
+	public BookTree() {
+		root = null;
 	}
 	
 	
+	/**
+	 * used to add a root to a empty tree. 
+	 * 
+	 * @param newRoot new root of the tree
+	 */
+	private void addRoot(Book newRoot) {
+		
+		this.root = newRoot;
+		newRoot.parent = null;
+		newRoot.rightChild = null;
+		newRoot.leftChild = null;
+		return;
+	}
 	
+	/**
+	 * This function searches the tree for a book using its ISBN number.
+	 * 
+	 * @param isbn the ISBN number of the book
+	 * 
+	 * @return null if the book is not in the database.
+	 * 			the pointer to the book object if it is.
+	 */
+	
+	public Book findBook(int isbn){
+	
+		Book searchBook = root;
+		
+		while(searchBook != null && searchBook.ISBN != isbn) {
+			if(searchBook.ISBN < isbn) {
+				searchBook = searchBook.leftChild;
+			} else {
+				searchBook = searchBook.rightChild;
+			}
+		}
+		
+		return searchBook;
+	}
+	
+	/**
+	 * this function adds  new book to the tree as a leaf.
+	 * 
+	 * @param book book to be added to the tree.
+	 */
+	public void addBook(Book book) {
+		
+		if (root == null) {
+			addRoot(book);
+			return;
+		}
+		
+		Book searchBook = root;
+		
+		while(searchBook.ISBN != book.ISBN) {
+			
+			if(searchBook.ISBN < book.ISBN) {
+				if(searchBook.leftChild == null){
+					searchBook.leftChild = book;
+					book.parent = searchBook;
+					return;
+				} else {
+					searchBook.leftChild = book;
+				}
+				
+			} else {
+				if(searchBook.rightChild == null){
+					searchBook.rightChild = book;
+					book.parent = searchBook;
+					return;
+				} else {
+					searchBook.rightChild = book;
+				}
+				
+			}
+		}
+		
+		return;
+	}
 }
--- User.java
+++ User.java
@@ -1,16 +1,35 @@
 public class User {
 
 	String userName;
-	String[] lentBooks;
-	String[] orderedBooks;
+	Book[] lentBooks;
+	Book[] orderedBooks;
 	
 	public User(String name) {
 		
 		this.userName = name;
+		lentBooks = new Book[0];
+		orderedBooks = new Book[0];
 		
 	}
+	/**
+	 * \\TODO commentary
+	 * 
+	 * @param book
+	 */
 	
-	/* public boolean searchOrderedBooks(String bookTitle) {
+	public void addBookToLendArry(Book book) {
+		Book[] newLentArray = new Book[lentBooks.length+1];
+		
+		for(int i = 0; i < lentBooks.length; i++) {
+			newLentArray[i] = lentBooks[i];
+		}
+		newLentArray[lentBooks.length] = book;
+		lentBooks = newLentArray;
+		
+		return;
+	}
+	
+	/* public boolean searchOrderedBooks(Book bookTitle) {
 	 * 
 	 *
 	 * 
